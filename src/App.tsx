df4ec8dfb9fb5c4379f240e36523d8ec1dff28a5
KingFish <>
2025-04-23 10:24:25 +0200

    Add routing for examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,25 @@
 import { FC } from "react"
-import { useObjectState } from "./hooks"
+import { NavLink, Outlet } from "react-router"
 
-enum Keys {
-  NAME = 'name',
-  AGE = 'age'
-}
+import './app.css';
 
 export const App: FC = () => {
-  const {
-    name,
-    age,
-    update,
-    reset,
-    errors,
-  } = useObjectState(
-    {
-      [Keys.NAME]: 'The builder',
-      [Keys.AGE]: 0
-    },
-    {
-      validators: {
-        name: (name) => name !== 'Bob' ? ['Not Bob'] : null,
-        age: (age) => age === 12 ? ['I dislike the number 12, pick another'] : null
-      },
-      htmlValidation: true,
-    }
-  );
-
-  const setCorrectName = () => {
-    update(Keys.NAME)('Bob')
-  }
-
   return (
     <>
-      <button onClick={setCorrectName}>Set correct name</button>
-      <hr />
-      <label htmlFor={Keys.NAME}>
-        Pick a nice name
-        <input
-          placeholder="Name please"
-          name={Keys.NAME}
-          type="text"
-          value={name}
-          maxLength={6}
-          onChange={update} />
-      </label>
-      {
-        errors.name && (
-          <p>{errors.name}</p>
-        )
-      }
-      <hr />
-      <label htmlFor={Keys.AGE}>
-        Pick a nice age
-        <input
-          placeholder="Age please"
-          name={Keys.AGE}
-          type="number"
-          min={5}
-          max={10}
-          value={age}
-          onChange={update} />
-      </label>
-      {
-          errors.age?.map((error) => (<p key={error}>{ error }</p>))
-      }
-      <hr />
-      <button onClick={reset}>Reset</button>
+      <nav>
+        <NavLink to="/" end>
+          None
+        </NavLink>
+        <NavLink to="/person-form" end>
+          Person form
+        </NavLink>
+        <NavLink to="/data-loader" end>
+          Data loader
+        </NavLink>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { DataLoader, PersonForm } from './components'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path='/person-form' index element={<PersonForm />} />
+          <Route path='/data-loader' index element={<DataLoader />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
